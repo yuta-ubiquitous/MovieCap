5bb564a8391169cfd44e46a5449a636c091eb388
Yuta Takahashi <yuta-ubiquitous@outlook.jp>
2016-05-02 04:21:56 +0900

    Movieを保存する機能を実装

--- app/src/main/java/com/ubiquitous/yuta/moviecap/MainActivity.java
+++ app/src/main/java/com/ubiquitous/yuta/moviecap/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ubiquitous.yuta.moviecap;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
@@ -10,16 +11,23 @@ import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,9 +38,20 @@ public class MainActivity extends AppCompatActivity {
     private CameraDevice cameraDevice;
     private CaptureRequest.Builder captureBuilder;
     private CameraCaptureSession cameraCaptureSession;
-
-    private TextureView cameraTextureView;
     private Size cameraPreviewSize;
+    private Size videoSize;
+
+    // record
+    private boolean isRecordingVideo;
+    private MediaRecorder mediaRecorder;
+    private Surface recorderSurface;
+
+    private Handler backgroundHandler;
+
+
+    // GUI
+    private TextureView cameraTextureView;
+    private Button recordButton;
 
     private boolean checkCameraHardware(Context context) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
@@ -59,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
                 streamConfigurationMap = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
 
                 cameraPreviewSize = streamConfigurationMap.getOutputSizes(SurfaceTexture.class)[0];
+
+                videoSize = chooseVideoSize(streamConfigurationMap.getOutputSizes(MediaRecorder.class));
 
                 // this.configureTransform();
 
@@ -88,6 +109,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private static Size chooseVideoSize(Size[] choices) {
+        for (Size size : choices) {
+            if (size.getWidth() == size.getHeight() * 4 / 3 && size.getWidth() <= 1080) {
+                return size;
+            }
+        }
+        return choices[choices.length - 1];
+    }
+
     protected void createCameraPreviewSession() {
         if (null == cameraDevice || !cameraTextureView.isAvailable() || null == cameraPreviewSize) {
             return;
@@ -111,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
             cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
                 @Override
                 public void onConfigured(CameraCaptureSession session) {
+                    Log.d(TAG, "onConfigured()");
                     cameraCaptureSession = session;
                     updatePreview();
                 }
@@ -125,18 +156,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void updatePreview(){
-        if(null == cameraDevice){
+    protected void updatePreview() {
+        if (null == cameraDevice) {
             return;
         }
 
         HandlerThread handlerThread = new HandlerThread("CameraPreview");
         handlerThread.start();
-        final Handler backgroundHandler = new Handler(handlerThread.getLooper());
+        backgroundHandler = new Handler(handlerThread.getLooper());
 
-        try{
+        try {
+            // Previewに描画
             cameraCaptureSession.setRepeatingRequest(captureBuilder.build(), null, backgroundHandler);
-        }catch(CameraAccessException e){
+        } catch (CameraAccessException e) {
             e.printStackTrace();
         }
 
@@ -166,15 +198,135 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private void prepareMediaRecorder() {
+
+        mediaRecorder = new MediaRecorder();
+
+        // mic
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+
+        mediaRecorder.setOutputFile(this.getVideoOutputPath());
+        mediaRecorder.setVideoEncodingBitRate(10000000);
+        mediaRecorder.setVideoFrameRate(30);
+        mediaRecorder.setVideoSize(videoSize.getWidth(), videoSize.getHeight());
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+
+        // set orientation?
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private String getVideoOutputPath() {
+        return this.getApplicationContext().getExternalFilesDir(null).getAbsolutePath() + "/" + System.currentTimeMillis() + ".mp4";
+    }
+
+    ;
+
+    private final Button.OnClickListener recordButtonListerner = new Button.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            if (isRecordingVideo) {
+                // stop Recording
+                isRecordingVideo = false;
+
+                mediaRecorder.stop();
+                mediaRecorder.reset();
+                createCameraPreviewSession();
+
+            } else {
+                // start Recording
+                isRecordingVideo = true;
+
+                Log.d(TAG, "start Recording");
+
+                if (cameraCaptureSession != null) {
+                    Log.d(TAG, "close preview session");
+                    cameraCaptureSession.close();
+                    cameraCaptureSession = null;
+                }
+
+                prepareMediaRecorder();
+
+                try {
+                    captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_RECORD);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+
+                List<Surface> surfaces = new ArrayList<Surface>();
+
+                SurfaceTexture surfaceTexture = cameraTextureView.getSurfaceTexture();
+                surfaceTexture.setDefaultBufferSize(cameraPreviewSize.getWidth(), cameraPreviewSize.getHeight());
+                Surface surface = new Surface(surfaceTexture);
+
+                surfaces.add(surface);
+                captureBuilder.addTarget(surface);
+
+                Surface mediaRecorderSurface = mediaRecorder.getSurface();
+                if (mediaRecorderSurface == null) {
+                    Log.d(TAG, "mediaRecorderSurface is null");
+                }
+                surfaces.add(mediaRecorderSurface);
+                captureBuilder.addTarget(mediaRecorderSurface);
+
+                HandlerThread handlerThread = new HandlerThread("VideoCapture");
+                handlerThread.start();
+                backgroundHandler = new Handler(handlerThread.getLooper());
+
+                Log.d(TAG, "setup backgroundHandler");
+
+                Log.d(TAG, "cameraDevice is " + (cameraDevice != null));
+
+                try {
+                    cameraDevice.createCaptureSession(surfaces, new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(CameraCaptureSession session) {
+                            cameraCaptureSession = session;
+                            updatePreview();
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mediaRecorder.start();
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onConfigureFailed(CameraCaptureSession session) {
+
+                        }
+                    }, backgroundHandler);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.camera_activity);
 
-        cameraTextureView = (TextureView) this.findViewById(R.id.cameraView);
+        isRecordingVideo = false;
 
+        cameraTextureView = (TextureView) this.findViewById(R.id.cameraView);
         cameraTextureView.setSurfaceTextureListener(previewStatusChagedListener);
+
+        recordButton = (Button) this.findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(recordButtonListerner);
 
         /*
         if (this.checkCameraHardware(this.getApplicationContext())) {
